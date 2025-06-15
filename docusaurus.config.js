295e459a2cb3e95700fda67b5b9129438e7ae76e
Spencer Pope <spenpo@spenpo.com>
2025-06-15 17:19:41 +0800

    some theme updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,10 +81,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Cracker',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Cracker Logo',
+          src: 'img/cracker.svg',
         },
         items: [
           {
